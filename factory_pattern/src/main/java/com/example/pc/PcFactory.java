52ed0ec93bce9ebb541c42fd95e4d170a55abe75
yufuworks <yufuworks@outlook.jp>
2022-03-27 15:35:01 +0000

    間違えてgit除外していたファイルをコミット

---
+++ factory_pattern/src/main/java/com/example/pc/PcFactory.java
@@ -0,0 +1,26 @@
+package com.example.pc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.factory.Factory;
+import com.example.factory.Product;
+
+public class PcFactory extends Factory {
+
+  private List<String> serialNumberList = new ArrayList();
+  
+  @Override
+  protected Product createProduct(String serialNumber) {
+    return new Pc(serialNumber);
+  }
+
+  @Override
+  protected void registerProduct(Product product) {
+    serialNumberList.add(((Pc) product).getSerialNumber());
+  }
+  
+  public List<String> getSerialNumberList() {
+    return serialNumberList;
+  }
+}
