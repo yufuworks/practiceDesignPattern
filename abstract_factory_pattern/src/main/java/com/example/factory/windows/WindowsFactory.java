4d6c3ed1d84befa5bc8ecdf3de10aaeb6273d406
yufuworks <yufuworks@outlook.jp>
2022-03-28 05:39:27 +0000

    AFP: 実装漏れクラスの追加

---
+++ abstract_factory_pattern/src/main/java/com/example/factory/windows/WindowsFactory.java
@@ -0,0 +1,21 @@
+package com.example.factory.windows;
+
+import com.example.factory.OsDisplaySystem;
+import com.example.factory.OsFactory;
+import com.example.factory.OsFileSystem;
+
+public class WindowsFactory extends OsFactory {
+
+	@Override
+	public OsFileSystem createFileSystem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OsDisplaySystem createDisplaySystem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+  
+}
